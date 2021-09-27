308a37a6cbf05be4fcdad1dd293fcc3b52eecfab
wilflingsederl190046 <wilflingsederl190046@sus.htl-grieskrichen.at>
2021-09-27 11:31:32 +0200

    finished with example one

--- src/HalloJavamitForEach.java
+++ src/HalloJavamitForEach.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class HalloJavamitForEach {
-    
+
+    public static void main(String[] args) {
+        List<String> liste = new ArrayList<>();
+
+        liste.add("Hallo");
+        liste.add("foo");
+        liste.add("test");
+        liste.add("123");
+
+        for(String s : liste) {
+            System.out.println(s);
+        }
+
+        System.out.println("");
+
+        liste.forEach((String s) -> System.out.println(s));
+
+        Consumer<String> consumer = (String s) -> System.out.println(s);
+
+        System.out.println("");
+
+        liste.forEach(System.out::println);
+    }
 }
---
+++ src/NumberTester.java
@@ -0,0 +1,26 @@
+public class NumberTester {
+
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+
+    public NumberTester(String fileName) {
+
+    }
+
+    public void setOddEventTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile() {
+
+    }
+}
