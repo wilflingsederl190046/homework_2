d014d4137d6a42f05fda27ef8ece6b9b9bc3dde5
wilflingsederl190046 <wilflingsederl190046@sus.htl-grieskrichen.at>
2021-09-29 08:37:01 +0200

    finished with RationalCalculator

--- src/example3/RationalCalculator.java
+++ src/example3/RationalCalculator.java
@@ -16,16 +16,25 @@ public class RationalCalculator extends AbstractCalculator {
 
     @Override
     public Number subtract(Number a, Number b) {
-        return null;
+        Number result = new Number();
+        result.setA((a.getA() * b.getB()) - (a.getB() * b.getA()));
+        result.setB(a.getB() * b.getB());
+        return result;
     }
 
     @Override
     public Number multiply(Number a, Number b) {
-        return null;
+        Number result = new Number();
+        result.setA(a.getA() * b.getA());
+        result.setB(a.getB() * b.getB());
+        return result;
     }
 
     @Override
     public Number divide(Number a, Number b) {
-        return null;
+        Number result = new Number();
+        result.setA(a.getA() * b.getB());
+        result.setB(a.getB() * b.getA());
+        return result;
     }
 }
