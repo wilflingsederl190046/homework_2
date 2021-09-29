6e8b07045db1b77c6ccc039948a742cc93621c4c
wilflingsederl190046 <wilflingsederl190046@sus.htl-grieskrichen.at>
2021-09-29 08:58:08 +0200

    begin with ComplexCalculator

--- src/example3/ComplexCalculator.java
+++ src/example3/ComplexCalculator.java
@@ -8,17 +8,27 @@ public class ComplexCalculator extends AbstractCalculator {
 
     @Override
     public Number add(Number a, Number b) {
-        return null;
+        Number result = new Number();
+        result.setA(a.getA() + b.getA());
+        result.setB(a.getB() + b.getB());
+        return result;
+
     }
 
     @Override
     public Number subtract(Number a, Number b) {
-        return null;
+        Number result = new Number();
+        result.setA(a.getA() - b.getA());
+        result.setB(a.getB() - b.getB());
+        return result;
     }
 
     @Override
     public Number multiply(Number a, Number b) {
-        return null;
+        Number result = new Number();
+        result.setA((a.getA() * b.getA()) - (a.getB() * b.getB()));
+        result.setB((a.getA() * b.getB()) + (a.getB() * b.getA()));
+        return result;
     }
 
     @Override
