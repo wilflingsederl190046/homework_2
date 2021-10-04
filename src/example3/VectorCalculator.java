e694022ae61cdfbe1fea106278275a49052ec491
wilflingsederl190046 <wilflingsederl190046@sus.htl-grieskrichen.at>
2021-10-04 10:21:45 +0200

    changed VectorCalculator

--- src/example3/VectorCalculator.java
+++ src/example3/VectorCalculator.java
@@ -7,14 +7,14 @@ public class VectorCalculator extends AbstractCalculator {
     }
 
     @Override
-    public Number add(Number a, Number b) { return null; }
+    public Number add(Number a, Number b) { return add.calc(a, b); }
 
     @Override
-    public Number subtract(Number a, Number b) { return null; }
+    public Number subtract(Number a, Number b) { return subtract.calc(a, b); }
 
     @Override
-    public Number multiply(Number a, Number b) { return null; }
+    public Number multiply(Number a, Number b) { return multiply.calc(a, b); }
 
     @Override
-    public Number divide(Number a, Number b) { return null; }
+    public Number divide(Number a, Number b) { return divide.calc(a, b); }
 }
