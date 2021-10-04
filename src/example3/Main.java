250017f770596daf5328ccd016e0570d65389fe4
wilflingsederl190046 <wilflingsederl190046@sus.htl-grieskrichen.at>
2021-10-04 10:18:37 +0200

    changed main

--- src/example3/Main.java
+++ src/example3/Main.java
@@ -3,96 +3,97 @@ package example3;
 import java.util.Scanner;
 
 public class Main {
-
-    private static AbstractCalculator rationalcalc;
-    private static AbstractCalculator vectorcalc;
-    private static AbstractCalculator complexcalc;
-
     public static void main(String[] args) {
 
-        rationalcalc.add = (Number a, Number b) -> {
+        CalculationOperation rationalAdd = (Number a, Number b) -> {
             Number result = new Number();
             result.setA((a.getA() * b.getB()) + (a.getB() * b.getA()));
             result.setB(a.getB() * b.getB());
             return result;
         };
 
-        rationalcalc.subtract = (Number a, Number b) -> {
+        CalculationOperation rationalSubtract = (Number a, Number b) -> {
             Number result = new Number();
             result.setA((a.getA() * b.getB()) - (a.getB() * b.getA()));
             result.setB(a.getB() * b.getB());
             return result;
         };
 
-        rationalcalc.multiply = (Number a, Number b) -> {
+        CalculationOperation rationalMultiply = (Number a, Number b) -> {
             Number result = new Number();
             result.setA(a.getA() * b.getA());
             result.setB(a.getB() * b.getB());
             return result;
         };
 
-        rationalcalc.divide = (Number a, Number b) -> {
+        CalculationOperation rationalDivide = (Number a, Number b) -> {
             Number result = new Number();
             result.setA(a.getA() * b.getB());
             result.setB(a.getB() * b.getA());
             return result;
         };
 
-        vectorcalc.add = (Number a, Number b) -> {
+        RationalCalculator rc = new RationalCalculator(rationalAdd, rationalSubtract, rationalMultiply, rationalDivide);
+
+        CalculationOperation vectorAdd = (Number a, Number b) -> {
             Number result = new Number();
             result.setA(a.getA() + b.getA());
             result.setB(a.getB() + b.getB());
             return result;
         };
 
-        vectorcalc.subtract = (Number a, Number b) -> {
+        CalculationOperation vectorSubtract = (Number a, Number b) -> {
             Number result = new Number();
             result.setA(a.getA() - b.getA());
             result.setB(a.getB() - b.getB());
             return result;
         };
 
-        vectorcalc.multiply = (Number a, Number b) -> {
+        CalculationOperation vectorMultiply = (Number a, Number b) -> {
             Number result = new Number();
             result.setA(a.getA() * b.getA());
             result.setB(a.getB() * b.getB());
             return result;
         };
 
-        vectorcalc.divide = (Number a, Number b) -> {
+        CalculationOperation vectorDivide = (Number a, Number b) -> {
             Number result = new Number();
             result.setA(a.getA() / b.getA());
             result.setB(a.getB() / b.getB());
             return result;
         };
 
-        complexcalc.add = (Number a, Number b) -> {
+        VectorCalculator vc = new VectorCalculator(vectorAdd, vectorSubtract, vectorMultiply, vectorDivide);
+
+        CalculationOperation complexAdd = (Number a, Number b) -> {
             Number result = new Number();
             result.setA(a.getA() + b.getA());
             result.setB(a.getB() + b.getB());
             return result;
         };
 
-        complexcalc.subtract = (Number a, Number b) -> {
+        CalculationOperation complexSubtract = (Number a, Number b) -> {
             Number result = new Number();
             result.setA(a.getA() - b.getA());
             result.setB(a.getB() - b.getB());
             return result;
         };
 
-        complexcalc.multiply = (Number a, Number b) -> {
+        CalculationOperation complexMultiply = (Number a, Number b) -> {
             Number result = new Number();
             result.setA((a.getA() * b.getA()) - (a.getB() * b.getB()));
             result.setB((a.getA() * b.getB()) + (a.getB() * b.getA()));
             return result;
         };
 
-        complexcalc.divide = (Number a, Number b) -> {
+        CalculationOperation complexDivide = (Number a, Number b) -> {
             Number result = new Number();
             result.setA((a.getA() * b.getA()) + (a.getB() * b.getB()));
             result.setB((a.getA() * b.getB()) - (a.getB() * b.getA()));
             return result;
         };
+
+        ComplexCalculator cc = new ComplexCalculator(complexAdd, complexSubtract, complexMultiply, complexDivide);
 
         Scanner s = new Scanner(System.in);
         while(true) {
@@ -110,15 +111,19 @@ public class Main {
                 System.out.println("Please enter only numbers from 1 to 4 and try again!\n");
             } else {
                 int operation = 0;
+                double numberxa = 0;
+                double numberxb = 0;
+                double numberya = 0;
+                double numberyb = 0;
                 do {
                     System.out.print("Enter number x a> ");
-                    int numberxa = s.nextInt();
+                    numberxa = s.nextInt();
                     System.out.print("Enter number x b> ");
-                    int numberxb = s.nextInt();
+                    numberxb = s.nextInt();
                     System.out.print("Enter number x a> ");
-                    int numberya = s.nextInt();
+                    numberya = s.nextInt();
                     System.out.print("Enter number x b> ");
-                    int numberyb = s.nextInt();
+                    numberyb = s.nextInt();
                     System.out.println("Choose operation:\n" +
                             "1 - add\n" +
                             "2 - subtract\n" +
@@ -129,14 +134,61 @@ public class Main {
                     operation = s.nextInt();
                 } while (operation == 5);
 
-                System.out.println("----------------");
+                Number number1 = new Number();
+                number1.setA(numberxa);
+                number1.setB(numberxb);
 
+                Number number2 = new Number();
+                number2.setA(numberya);
+                number2.setB(numberyb);
+
+                Number result = null;
                 switch (userInput) {
                     case 1:
-
-                    default:
-
+                        if(operation == 1) {
+                            result  = rc.add(number1, number2);
+                        } else if(operation == 2) {
+                            result  = rc.subtract(number1, number2);
+                        } else if(operation == 3) {
+                            result  = rc.multiply(number1, number2);
+                        }  else if(operation == 4) {
+                            result  = rc.divide(number1, number2);
+                        } else {
+                            throw new IllegalArgumentException("Only Enter Number from 1 to 5!");
+                        }
+                        break;
+                    case 2:
+                        if(operation == 1) {
+                            result  = vc.add(number1, number2);
+                        } else if(operation == 2) {
+                            result  = vc.subtract(number1, number2);
+                        } else if(operation == 3) {
+                            result  = vc.multiply(number1, number2);
+                        }  else if(operation == 4) {
+                            result  = vc.divide(number1, number2);
+                        } else {
+                            throw new IllegalArgumentException("Only Enter Number from 1 to 5!");
+                        }
+                        break;
+                    case 3:
+                        if(operation == 1) {
+                            result  = cc.add(number1, number2);
+                        } else if(operation == 2) {
+                            result  = cc.subtract(number1, number2);
+                        } else if(operation == 3) {
+                            result  = cc.multiply(number1, number2);
+                        }  else if(operation == 4) {
+                            result  = cc.divide(number1, number2);
+                        } else {
+                            throw new IllegalArgumentException("Only Enter Number from 1 to 5!");
+                        }
                 }
+
+                System.out.println("--------------\n" +
+                        "Solution:\n" +
+                        "a = " + result.getA() + "\n" +
+                        "b = " + result.getB() + "\n" +
+                        "--------------");
             }
         }
     }
